7da943719b8df5c57b52cb39cf83ba3204e50848
anu-bazar <anubazarragcaa@gmail.com>
2024-04-19 11:45:24 +0200

    13 update title

--- pages/13_Climate_Action.py
+++ pages/13_Climate_Action.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit.components.v1 as components
 
-st.title("Mongolian SDG Tracker - Demo")
+st.title("Mongolian SDG Tracker - Goal 13")
 
 
 st.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
