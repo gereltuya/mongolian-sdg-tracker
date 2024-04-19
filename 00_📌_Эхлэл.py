cf3d403d38bc11857fbc439444356c1e6c3d85ef
anu-bazar <anubazarragcaa@gmail.com>
2024-04-19 09:52:01 +0200

    fixed title page

--- 00_📌_Эхлэл.py
+++ 00_📌_Эхлэл.py
@@ -2,26 +2,34 @@ import streamlit as st
 from PIL import Image
 import base64
 from io import BytesIO
+import textwrap
+
+st.set_page_config(
+    page_title="Эхлэл",
+    page_icon="📌",
+)
+
+st.sidebar.header("Mongolian SDG Tracker App Demo")
 
 def image_to_base64(image):
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     return base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-import textwrap
-
 def create_clickable_icon(image_path, alt_text, title, index):
-    page_route = f"pages/{str(index+1).zfill(2)}_{title.replace(' ', '_')}.py"
+    page_route = f"./{title.replace(' ', '_')}"
     encoded_image = image_to_base64(Image.open(image_path))
 
     wrapped_title = '<br>'.join(textwrap.wrap(title, width=20))
 
     st.markdown(f"""
-        <div style="text-align: center; margin: 20px;">
-            <a style="display: inline-block;" href="{page_route}">
-                <img src="data:image/png;base64,{encoded_image}" alt="{alt_text}" style="width:100%; max-width:150px; height:auto;">
-                <p style="max-width:150px; margin-top:5px;">{wrapped_title}</p>
-            </a>
+        <div style="text-align: center; margin-bottom: 20px;">
+            <div style="display: inline-block; text-align: center;">
+                <a href="{page_route}" style="text-decoration: none;">
+                    <img src="data:image/png;base64,{encoded_image}" alt="{alt_text}" style="width:150px;">
+                </a>
+                <p style="max-width: 50px; margin-top: 5px; margin-left:50px; text-align: center; font-weight: bold;">{wrapped_title}</p>
+            </div>
         </div>
         """, unsafe_allow_html=True)
 
@@ -31,7 +39,7 @@ def create_clickable_icon(image_path, alt_text, title, index):
 st.title('Sustainable Development Goals')
 st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
 
-cols_per_row = 3
+cols_per_row = 4 
 rows = (17 + cols_per_row - 1) // cols_per_row 
 
 english_titles = [
@@ -80,7 +88,8 @@ for row_num in range(rows):
             icon_path = f'assets/sdg-goals-icons/{str(index+1).zfill(2)}.png'
             alt_text = f'SDG Goal {index+1}'
             title = english_titles[index]  #use eng title (can be substituted for mongolian)
-            page_file_path = f"pages/{str(index+1).zfill(2)}_{title.replace(' ', '_')}.py" 
+            page_file_path =  f"./{title.replace(' ', '_')}"
+
             
             with cols[col_num]:
                 encoded_image = image_to_base64(Image.open(icon_path))
